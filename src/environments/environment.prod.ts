9c8318f0737ec52c3257b1ee655f653756e96909
abhayza <abhay.kumar@zeksta.com>
2022-05-03 11:47:59 +0530

    1117

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  requestUrl: 'https://dashboard.heroku.com/apps/localization-demo-backend:9091',
+  siteUrl: 'https://dashboard.heroku.com/apps/localization-fronted-demo//dist',
 };
